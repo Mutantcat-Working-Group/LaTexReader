f5b75ef17d8a8dad5c4d9c62029bd90f5207418a
孙达明 <83948628+tyza66@users.noreply.github.com>
2025-11-13 10:16:57 +0800

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -64,6 +64,7 @@
                 // 添加样式和脚本
                 if (!document.querySelector('#latex-styles')) {
                     // const styles = generator.stylesAndScripts("https://cdn.jsdelivr.net/npm/latex.js@0.12.4/dist/");
+                    // 使用自托管的样式文件以防 CDN 失效
                     const styles = generator.stylesAndScripts("https://latexreader.mutantcat.org/latex_base");
                     styles.id = 'latex-styles';
                     document.head.appendChild(styles);
